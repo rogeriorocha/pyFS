bd25efbeb9161cd9bdf8454a99a09d17f704c526
Rogerio Rocha <rogeriosilvarocha@gmail.com>
2019-09-17 18:32:58 -0300

    db confs OK

--- application/service.py
+++ application/service.py
@@ -13,14 +13,25 @@ def upload(file):
     
     codArq = ad.cod_arq
 
-    print(codArq)
-
     fs = FileStorage(codArq)
     filename = secure_filename(file.filename)
     #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     os.makedirs(fs.path, exist_ok=True)
     file.save(fs.file)
+    
+    
+
+
     hashFileMD5 = HashUtils.getFileMD5(fs.file)
+
+    #fileSize = os.path.getsize(file)
+    #ad.tam_arq = fileSize
+    ad.nom_orig = filename;
+    ad.cod_algtm_hash = hashFileMD5;
+
+    db.session.flush();
+    db.session.commit();
+
     print(fs.exists())
     
     with open(fs.fileHash, 'w') as fileMd5:
